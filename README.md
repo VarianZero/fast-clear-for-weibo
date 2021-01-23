e5aa4da94daf644a99ec0a307eb615793239ddfb
wangjinquan <wangjinquan@chinamobile.com>
2021-01-23 16:33:50 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # fast-clear-for-weibo
+
 A chrome extension for remove all follow! Based on batch manage of weibo.
+
+# Usage
+
+0. 请确保 Chrome 版本已升级至 88+版本，因为插件采用了 V3 版本，仅支持 88 版本以上！
+1. 打开 Chrome 扩展列表: chrome://extensions/ and
+2. 点击右侧开发者模式，然后点击加载已解压的扩展程序
+3. 打开浏览器，切换到微博页面，点击关注
+4. 点击插件，在小弹窗中点击取消关注
+
+# Attention
+
+1. 未做任何筛选优化，只支持全部删除，一旦删除，不可恢复！
+2. 取关接口有一定响应时间，点击一次后等待页面刷新结果后即可，不要连续点击！
---
+++ popup.js
@@ -0,0 +1,23 @@
+let clearBtn = document.getElementById('clearAllFollow');
+
+
+clearBtn.onclick = function (element) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: clearAll
+    });
+  });
+};
+
+function clearAll() {
+  if (/weibo.com[\w\d]*follow/.test(window.location.host)) {
+    document.querySelector(".btn_link.S_txt1").click();
+    document.querySelectorAll('.member_li').forEach(l => l.click());
+    document.querySelector('.W_btn_a[node-type="cancelFollowBtn"]').click();
+    document.querySelector('[node-type="ok"]').click();
+  } else {
+    alert('请在微博管理关注页面使用');
+  }
+
+}
